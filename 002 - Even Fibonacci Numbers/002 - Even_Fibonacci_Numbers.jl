6e61df9c8a597123304064d03e262c70b3182d42
Umut Ozel <umutozel@gmail.com>
2017-01-11 09:24:15 +0200

    Improving sum logic

--- 002 - Even Fibonacci Numbers/002 - Even_Fibonacci_Numbers.jl
+++ 002 - Even Fibonacci Numbers/002 - Even_Fibonacci_Numbers.jl
@@ -1,2 +1,2 @@
-fibber(pre, cur, n) = cur>=4e6 ? n+pre : fibber(cur, pre+cur, pre%2 == 1 ? n : n+pre)
+fibber(pre, cur, n) = cur>4e6 ? n : fibber(cur, pre+cur, cur%2 == 1 ? n : n+cur)
 println(fibber(1, 2, 0))
